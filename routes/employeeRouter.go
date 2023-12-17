b9de16e6a9764bc660b6d0f3e00067fc49baf760
kushan <kushansuk2@gmail.com>
2023-12-18 03:33:27 +0530

    added all routes

--- routes/employeeRouter.go
+++ routes/employeeRouter.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"hrms/handlers"
 	"gofr.dev/pkg/gofr"
 )
 
 func EmployeeRoutes(app *gofr.Gofr) {
-	app.GET("/employee",)
+	app.GET("/employee",handlers.GetAllEmployees())
+	app.POST("/employee",handlers.CreateEmployee())
+	app.PUT("/employee/{id}",handlers.UpdateEmployee())
+	app.DELETE("/employee/{id}",handlers.DeleteEmployee())
 }
